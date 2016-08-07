4e1a132fd0fcae1a3d72dc5f2462b01ed8de9472
Tanner Heffner <tannerjheffner@gmail.com>
2016-08-06 17:18:54 -0700

    parse page data and push into csv file

--- web_scraper.rb
+++ web_scraper.rb
@@ -1,9 +1,25 @@
 require 'HTTParty'
 require 'Nokogiri'
 require 'JSON'
-require 'PRY'
 require 'csv'
 
-page = HTTParty.get('https://portland.craigslist.org/search/mis?s=0')
+# this is how we request the page we're going to scrape
+page = HTTParty.get('https://portland.craigslist.org/search/bar?s=100')
 
-Pry.start(binding)
+# transform our http response into nokogiri object
+parse_page = Nokogiri::HTML(page)
+
+# empty array to store the data we want from the scrape
+page_array = []
+
+# examine page to see where the data you want is located in the dom
+# parse that data and push to usable array
+parse_page.css('.content').css('.row').css('.hdrlnk').map do |a|
+    post_name = a.text
+    page_array.push(post_name)
+end
+
+# push the array into a CSV file
+CSV.open('data.csv', 'w') do |csv|
+    csv << page_array
+end
